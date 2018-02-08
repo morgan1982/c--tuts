c14df6e99e92bd1ab39ff6851b35ee20e557daf1
entropy82 <froutopotos.alex@gmail.com>
2018-02-08 16:40:26 +0200

    add second object

--- hello.cpp
+++ hello.cpp
@@ -8,6 +8,11 @@ using namespace std;
 class Concept {
 
     public:
+        Concept (string z) {
+
+            setName(z);
+            cout << "this is the constuctor running" << endl;
+        }
         void setName(string x) {
             name = x;
         }
@@ -26,9 +31,11 @@ class Concept {
 
 int main()
 {
-    Concept bo;
-    bo.setName("einai o tom?");
+    Concept bo("eimai o huan ramon!");
+    // bo.setName("einai o tom?");
     cout << bo.getName() << endl;
+    Concept bo2("second object");
+    cout << bo2.getName() << endl;
 
 
     return 0;
