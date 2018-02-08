75f487447307d935d5271b9ce4113e4673176e44
entropy82 <froutopotos.alex@gmail.com>
2018-02-08 13:12:56 +0200

    add the obejt

--- hello.cpp
+++ hello.cpp
@@ -3,27 +3,25 @@
 using namespace std;
 
 
-void trompeta();
-void print_int(int x);
-int sum(int x, int y);
+
+class Concept {
+
+    public:
+        void just_init() {
+            cout << "boom!" << endl;
+        }
+
+
+};
+
+
+
 
 int main()
 {
+    Concept concept_class; //creation of the object
+    concept_class.just_init();
 
-    kolion();
-    print_int(20);
-    cout << "sum is " << sum(20, 40) << endl;
     return 0;
 }
 
-
-void trompeta() {
-    cout << "what is this!" << endl;
-}
-void print_int(int x) {
-    cout << "num is " << x << endl;
-}
-int sum(int x, int y) {
-    int total = x + y;
-    return total;
-}
